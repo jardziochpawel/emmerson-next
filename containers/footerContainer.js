d3d3fb4c036ce2267b4a85c0428e9c53c377d9c5
paweljardzioch <7eScf7E3>
2021-08-01 15:51:02 +0200

    LCP

--- containers/footerContainer.js
+++ containers/footerContainer.js
@@ -2,8 +2,11 @@ import {Footer, Header} from '../components';
 import {ROUTES} from "../constants/routes";
 import React from "react";
 import Link from 'next/link';
+import {useWebPSupportCheck} from "react-use-webp-support-check";
 
 export default function FooterContainer({diagonal = false}){
+    const webp = useWebPSupportCheck();
+
     const renderNavFooter = () => {
         const array = [];
         ROUTES.mainMenu.map(item =>{
@@ -18,7 +21,7 @@ export default function FooterContainer({diagonal = false}){
             <Footer diagonal={diagonal}>
                 <Footer.Container>
                     <Footer.Column>
-                        <img src='/images/misc/logotyp-footer.png' alt='Emmerson Zarządzanie sp z o.o.' /><br/><br/>
+                        <img src={webp ? '/images/misc/logotyp-footer.webp' : '/images/misc/logotyp-footer.png'} alt='Emmerson Zarządzanie sp z o.o.' width='184' height='45' /><br/><br/>
                         Emmerson Zarządzanie Sp. z o.o.<br/>
                         ul.Zgrupowania AK Kampinos 2,<br/>
                         01-943 Warszawa<br/>
