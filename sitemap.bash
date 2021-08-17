281873b0558bb4b5f0069691b368848e394c7abb
paweljardzioch <7eScf7E3>
2021-08-17 19:59:28 +0200

    add colors to sitemap.bash add install tput alpine linux in Dockerfile

--- sitemap.bash
+++ sitemap.bash
@@ -1,6 +1,13 @@
 #!/bin/bash
 SECONDS=0
 
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+GREEN=$(tput setaf 2)
+RED=$(tput setaf 1)
+YELLOW=$(tput setaf 3)
+BLUE=$(tput setaf 4)
+
 SITEMAP_INDEX=(offers common)
 
 cd public && rm -rf sitemap && mkdir "sitemap"
