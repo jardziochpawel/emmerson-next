df32ea089d35c4a886c41016cc0e2b668f43d793
paweljardzioch <7eScf7E3>
2021-08-18 12:29:48 +0200

    change Dockerfile

--- sitemap.bash
+++ sitemap.bash
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-mkdir public/sitemap
+[ ! -d "./public/sitemap" ] && mkdir public/sitemap
 rm -f public/sitemap/*
 
 for value in offers common
@@ -9,11 +9,13 @@ do
 done
 
 node ./compress-sitemap.js
+
 cd public/sitemap || exit
 
 for value in offers common
 do
-  rm -rf sitemap-"${SITEMAP}".xml
+  echo "removing sitemap-${value}.xml file"
+  rm -rf sitemap-"${value}".xml
 done
 
 cd ../../
